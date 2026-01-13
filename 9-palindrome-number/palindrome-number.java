eebf5904fde6dcec58719aa37067738fcf114dbe
Vatsalya Mishra <vatsalyamishra2005@gmail.com>
2026-01-14 00:00:58 +0530

    Time: 5 ms (72.56%) | Memory: 46.2 MB (21.11%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+        {
+        return false;
+    }
+    int temp=x;
+    int reverse=0;
+    while(x!=0){
+        reverse= (reverse*10)+(x%10);
+        x/=10;
+    }
+    if(temp==reverse)
+    {
+    return true;
+    }
+    else
+    return false;
+
+    }
+
+}
